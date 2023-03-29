4e9a2e5b0c06bd7a4b0953de52c7ebb2df02e84c
Saurav <sp.gharti@gmail.com>
2023-03-29 16:20:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-rice-2",
+  url: "https://hidden-rice-2.botics.co",
+  api: "https://hidden-rice-2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
